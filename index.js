c99baa1584eceb70d66007e2267c5f916c9e113c
Harry <Harry_emmett@outlook.com>
2022-02-03 13:05:54 +0000

    FNP-23 #comment added first GET requests

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/test', routes);
+
+const server = app.listen(5015, () => {
+    console.log('server started');
+});
+
+module.exports = server;
