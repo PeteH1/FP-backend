f5029370f691b56dd0a84a31d7ada68037e32b04
Pete <pjdhutchison@gmail.com>
2022-02-08 14:26:46 +0000

    Added queries for citizenId endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -34,14 +34,14 @@ router.get(`/:name`, async (req, res, next) => {
     const firstName = req.params.name.split(' ')[0];
     const surname = req.params.name.split(' ')[1];
     try {
-        const bioInfo = await sequelize.query(`SELECT * FROM DBFinalProj.citizen where forenames like ? and surname = ?; `, { replacements: [`%${firstName}%`, surname],  type: sequelize.QueryTypes.SELECT })
+        const bioInfo = await sequelize.query(`SELECT * FROM DBFinalProj.citizen where forenames like ? and surname = ?; `, { replacements: [`%${firstName}%`, surname], type: sequelize.QueryTypes.SELECT })
         res.status(201).send(bioInfo);
     } catch (error) {
         res.status(500).send({
             message:
                 error.message || "An error occcurred while retrieving data"
-            });
-        };
+        });
+    };
 });
 
 
@@ -50,31 +50,72 @@ router.get(`/:citizenId`, async (req, res, next) => {
     const citizenId = req.params.citizenId;
 
     try {
-        const a = await sequelize.query(
-            
-            `  ;`, { replacements: citizenId,  type: sequelize.QueryTypes.SELECT })
+        const citizenPassport = await sequelize.query(
 
-        const b = await sequelize.query(
-            
-            `  ;`, { replacements: citizenId,  type: sequelize.QueryTypes.SELECT })
+            `SELECT c.citizen_id, c.forenames, c.surname, c.home_address, c.date_of_birth, c.place_of_birth, c.sex, p.passport_number, p.nationality, p.issuing_country, p.date_of_issue, p.date_of_expiry
+            FROM DBFinalProj.citizen c 
+            JOIN DBFinalProj.passport p ON c.forenames=p.given_name AND c.surname=p.surname AND c.date_of_birth = p.dob
+            WHERE c.citizen_id=?;`, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
 
-        const c = await sequelize.query(
-            
-            `  ;`, { replacements: citizenId,  type: sequelize.QueryTypes.SELECT })
+        const callsReceived = await sequelize.query(
 
-        const d = await sequelize.query(
-            
-            `  ;`, { replacements: citizenId,  type: sequelize.QueryTypes.SELECT })
+            `SELECT mcr.timestamp, mcr.caller_msisdn, mcr.receiver_msisdn, ct.latitude, ct.longitude FROM DBFinalProj.mobilecallrecords mcr
+            JOIN DBFinalProj.celltower ct ON mcr.receiver_tower_id=ct.cell_tower_id
+            WHERE mcr.receiver_msisdn=(SELECT phone_number FROM DBFinalProj.peoplemobile pm 
+            JOIN DBFinalProj.citizen c ON c.forenames=pm.forenames AND c.surname=pm.surname AND c.date_of_birth=pm.date_of_birth AND c.home_address=pm.address
+            WHERE citizen_id=?)
+            GROUP BY timestamp ORDER BY timestamp desc;`, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
 
-        res.status(201).send({a, b, c, d});
+        const callsMade = await sequelize.query(
+
+            `SELECT mcr.timestamp, mcr.caller_msisdn, mcr.receiver_msisdn, ct.latitude, ct.longitude FROM DBFinalProj.mobilecallrecords mcr
+            JOIN DBFinalProj.celltower ct ON mcr.call_cell_tower_id=ct.cell_tower_id
+            WHERE mcr.caller_msisdn=(SELECT phone_number FROM DBFinalProj.peoplemobile pm 
+            JOIN DBFinalProj.citizen c ON c.forenames=pm.forenames AND c.surname=pm.surname AND c.date_of_birth=pm.date_of_birth AND c.home_address=pm.address
+            WHERE citizen_id=?)
+            GROUP BY timestamp ORDER BY timestamp desc;`, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
+
+        const anprSightings = await sequelize.query(
+
+            `SELECT vo.vehicle_registration_no, vr.make, vr.model, vr.colour, vo.timestamp, a.latitude, a.longitude FROM DBFinalProj.vehicleobservations vo
+            JOIN DBFinalProj.anprcamera a ON vo.anpr_point_id = a.anpr_id
+            JOIN DBFinalProj.vehicleregistration vr ON vo.vehicle_registration_no = vr.vehicle_registration_no
+            JOIN DBFinalProj.citizen c ON c.forenames = vr.forenames AND c.surname = vr.surname AND c.home_address = vr.address AND c.date_of_birth = vr.date_of_birth
+            WHERE c.citizen_id=?;`, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
+
+        const eposTransactions = await sequelize.query(
+
+            `SELECT pba.forenames, pba.surname, pba.account_number, pba.bank, bc.sort_code, epost.bank_card_number, epost.timestamp, epost.payee_account, epost.amount,
+            epos.vendor, epos.street_name, epos.postcode, epos.latitude, epos.longitude FROM DBFinalProj.citizen c
+            JOIN DBFinalProj.peoplebankaccount pba ON c.forenames=pba.forenames AND c.surname=pba.surname AND c.home_address=pba.home_address AND c.date_of_birth=pba.date_of_birth
+            JOIN DBFinalProj.bankcard bc ON pba.account_number=bc.account_number
+            JOIN DBFinalProj.epostransactions epost ON bc.card_number=epost.bank_card_number
+            JOIN DBFinalProj.epos ON epost.epos_id=epos.id
+            WHERE c.citizen_id=?;`, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
+
+        const atmTransactions = await sequelize.query(
+
+            `SELECT pba.forenames, pba.surname, pba.account_number, pba.bank, bc.sort_code, atmt.bank_card_number, atmt.timestamp, atmt.type, atmt.amount,
+            atmp.operator, atmp.street_name, atmp.postcode, atmp.latitude, atmp.longitude FROM DBFinalProj.peoplebankaccount pba
+            JOIN DBFinalProj.citizen c ON c.forenames=pba.forenames AND c.surname=pba.surname AND c.home_address=pba.home_address AND c.date_of_birth=pba.date_of_birth
+            JOIN DBFinalProj.bankcard bc ON pba.account_number=bc.account_number
+            JOIN DBFinalProj.atmtransaction atmt ON bc.card_number=atmt.bank_card_number
+            JOIN DBFinalProj.atmpoint atmp ON atmt.atm_id=atmp.atm_id
+            WHERE c.citizen_id=?;`, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
+
+        const eposTransactions = await sequelize.query(
+
+            ``, { replacements: citizenId, type: sequelize.QueryTypes.SELECT })
+
+        res.status(201).send({ citizenPassport, callsReceived, callsMade, anprSightings, eposTransactions, atmTransactions });
 
     } catch (error) {
         res.status(500).send({
             message:
                 error.message || "An error occcurred while retrieving data"
 
-            });
-        };
+        });
+    };
 });
 
 router.get(`/PeteHutchison`, async (req, res, next) => {
@@ -130,8 +171,8 @@ router.get(`/PeteHutchison`, async (req, res, next) => {
 //     await sequelize.query(` `, { replacements: [],  type: sequelize.QueryTypes.SELECT })
 
 //     await sequelize.query(` `, { replacements: [],  type: sequelize.QueryTypes.SELECT })
-        
-    
+
+
 //     .then((result) => {
 //             res.status(201).send(result); 
 //         })
@@ -179,7 +220,7 @@ router.get(`/PeteHutchison`, async (req, res, next) => {
 //     // const name = req.params.forename;
 //     const bioinfo = await sequelize.query(
 //         `SELECT * FROM DBFinalProj.citizen WHERE forenames='Peter' AND surname='Hutchison';`, { type: sequelize.QueryTypes.SELECT }); //TYPE: INCLUDED AS WILL DOUBLE THE RETURNED RESULTS FOR WHATEVER REASON
-    
+
 //         const transactions = await sequelize.query(
 //         `SELECT p.forenames, p.surname, ep.vendor, b.card_number, p.home_address, e.bank_card_number FROM DBFinalProj.peoplebankaccount p
 //         JOIN DBFinalProj.bankcard b ON p.account_number = b.account_number
@@ -208,18 +249,18 @@ router.get(`/PeteHutchison`, async (req, res, next) => {
 //         JOIN DBFinalProj.atmpoint atmp
 //         ON atmt.atm_id = atmp.atm_id
 //         WHERE atmt.bank_card_number = '9854539636224593';`, { type: sequelize.QueryTypes.SELECT });
-    
+
 //     res.status(201).send({ bioinfo, transactions, calls, whereabouts })
 
-    // .then((result) => {
-    //     res.status(201).send(result);
-    // })
-    // .catch((err) => {
-    //     res.status(500).send({
-    //         message:
-    //             err.message || "An error occcurred while retrieving data"
-    //     });
-    // });
+// .then((result) => {
+//     res.status(201).send(result);
+// })
+// .catch((err) => {
+//     res.status(500).send({
+//         message:
+//             err.message || "An error occcurred while retrieving data"
+//     });
+// });
 
 
 
